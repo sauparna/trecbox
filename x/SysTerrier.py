e9adb3dfed2410c400d0b4e9ada93e760e85b14c
Rup <sauparna.palc@gmail.com>
2014-01-08 15:52:44 -0500

    SysTerrier prints a notification everytime it indexes, retrieves or evaluates. This leads to neater setup.py code.

--- x/SysTerrier.py
+++ x/SysTerrier.py
@@ -8,12 +8,13 @@ class SysTerrier():
         self.env = env
         #self.model_map   = {"bm25": "BM25", "dfr": "DFI0", "tfidf": "TF_IDF"}
 
-        self.model_map = {"bb2": "BB2", "bm25": "BM25", "dfi0": "DFI0", "dfr_bm25": "DFR_BM25",
-                          "dlh": "DLH", "dlh13": "DLH13", "dph": "DPH", "dfree": "DFRee", 
-                          "hiemstra_lm": "Hiemstra_LM", "ifb2": "IFB2", "in_expb2": "In_expB2", 
-                          "in_expc2": "In_expC2", "inl2": "InL2", "lemurtf_idf": "LemurTF_IDF", 
-                          "lgd": "LGD", "pl2": "PL2", "tf_idf": "TF_IDF", 
-                          "dfrweightingmodel": "DFRWeightingModel"}
+        self.model_map = {"bb2": "BB2", "bm25": "BM25", "dfi0": "DFI0", "dfr_bm25": "DFR_BM25", 
+                          "dfree": "DFRee", "dfrweightingmodel": "DFRWeightingModel", 
+                          "dirichletlm": "DirichletLM", "dlh": "DLH", "dlh13": "DLH13", "dph": "DPH", 
+                          "hiemstra_lm": "Hiemstra_LM", "idf": "Idf", "ifb2": "IFB2", 
+                          "in_expb2": "In_expB2", "in_expc2": "In_expC2", "inb2": "InB2", 
+                          "inl2": "InL2", "js_kls": "Js_KLs", "lemurtf_idf": "LemurTF_IDF", 
+                          "lgd": "LGD", "pl2": "PL2", "tf_idf": "TF_IDF", "xsqra_m": "XSqrA_M"}
 
         self.stemmer_map = {"porter": "PorterStemmer", "weak-porter": "WeakPorterStemmer", 
                             "snowball": "EnglishSnowballStemmer"}
@@ -73,7 +74,9 @@ class SysTerrier():
 
 
     def index(self, itag, doc, opt):
-        
+
+        print itag
+
         pipeline, stopwords = self.__build_termpipeline(opt)
         i_file  = self.__write_doclist(itag, doc)
         o_dir   = os.path.join(self.env["index"], itag)
@@ -111,6 +114,8 @@ class SysTerrier():
             f.write(log)
 
     def retrieve(self, itag, rtag, opt, m, q):
+
+        print rtag
 
         pipeline, stopwords = self.__build_termpipeline(opt)
         i_dir = os.path.join(self.env["index"], itag)
@@ -155,6 +160,8 @@ class SysTerrier():
             f.write(log)
 
     def evaluate(self, rtag, qrels):
+
+        print rtag
 
         # trec_eval -q QREL_file Retrieval_Results > eval_output
 
--- x/setup.py
+++ x/setup.py
@@ -27,10 +27,10 @@ env = {
     "attic"   : os.path.join(root, "attic")
     }
 
-models = ["bb2", "bm25", "dfr_bm25", "dlh", "dlh13", 
-          "hiemstra_lm", "ifb2", "in_expb2", "in_expc2", 
-          "inl2", "lemurtf_idf", "pl2", 
-          "tf_idf", "pontecroft", "dfrweightingmodel"]
+models = ["bb2","bm25","dfi0","dfr_bm25","dfree","dfrweightingmodel",
+          "dirichletlm","dlh","dlh13","dph","hiemstra_lm","idf",
+          "ifb2","in_expb2","in_expc2","inb2","inl2","js_kls",
+          "lemurtf_idf","lgd", "pl2", "tf_idf","xsqra_m"]
 
 stemmers = ["porter", "weak-porter", "snowball"]
 
@@ -56,25 +56,102 @@ def tests():
     #s.retrieve("xyz", "xyz", ["None", "None"], "tfidf", t.query("terrier"))
     #s.evaluate("xyz", qrels["test1"])
 
-def exp(opt):
-    t = Topics(topics["t678"], "d")
+def exp_t678(opt):
+
     s = SysTerrier(env)
 
     if opt == "i":
-        s.index("t678", doc["t678"], ["stopwords", "None"])
-        #s.index("t678.p", doc["t678"], ["stopwords", "porter"])
-        #s.index("t678.wp", doc["t678"], ["stopwords", "weak-porter"])
-        #s.index("t678.s", doc["t678"], ["stopwords", "snowball"])
+
+        s.index("t678.n",  doc["t678"], ["stopwords", "None"])
+        s.index("t678.p",  doc["t678"], ["stopwords", "porter"])
+        s.index("t678.wp", doc["t678"], ["stopwords", "weak-porter"])
+        s.index("t678.s",  doc["t678"], ["stopwords", "snowball"])
+
     elif opt == "r":
-        s.retrieve("t678", "t678.bm25", ["stopwords", "None"], "bm25", t.query("terrier"))
+
+        t = Topics(topics["t678"], "d")
+        q = t.query("terrier")
+
+        for m in models:
+            s.retrieve("t678.n",  "t678.n."  + m, ["stopwords", "None"],        m, q)
+            s.retrieve("t678.p",  "t678.p."  + m, ["stopwords", "porter"],      m, q)
+            s.retrieve("t678.wp", "t678.wp." + m, ["stopwords", "weak-porter"], m, q)
+            s.retrieve("t678.s",  "t678.s."  + m, ["stopwords", "snowball"],    m, q)
+
     elif opt == "e":
-        s.evaluate("t678.bm25", qrels["t678"])
+
+        for m in models:
+            s.evaluate("t678.n."  + m, qrels["t678"])
+            s.evaluate("t678.p."  + m, qrels["t678"])
+            s.evaluate("t678.wp." + m, qrels["t678"])
+            s.evaluate("t678.s."  + m, qrels["t678"])
+
+def exp_t678_fr(opt):
+
+    s = SysTerrier(env)
+
+    if opt == "i":
+
+        s.index("t678-fr.n",  doc["t678-fr"], ["stopwords", "None"])
+        s.index("t678-fr.p",  doc["t678-fr"], ["stopwords", "porter"])
+        s.index("t678-fr.wp", doc["t678-fr"], ["stopwords", "weak-porter"])
+        s.index("t678-fr.s",  doc["t678-fr"], ["stopwords", "snowball"])
+
+    elif opt == "r":
+
+        t = Topics(topics["t678"], "d")
+        q = t.query("terrier")
+
+        for m in models:
+            s.retrieve("t678-fr",    "t678-fr."    + m, ["stopwords", "None"],        m, q)
+            s.retrieve("t678-fr.p",  "t678-fr.p."  + m, ["stopwords", "porter"],      m, q)
+            s.retrieve("t678-fr.wp", "t678-fr.wp." + m, ["stopwords", "weak-porter"], m, q)
+            s.retrieve("t678-fr.s",  "t678-fr.s."  + m, ["stopwords", "snowball"],    m, q)
+
+    elif opt == "e":
+
+        for m in models:
+            s.evaluate("t678-fr."    + m, qrels["t678"])
+            s.evaluate("t678-fr.p."  + m, qrels["t678"])
+            s.evaluate("t678-fr.wp." + m, qrels["t678"])
+            s.evaluate("t678-fr.s."  + m, qrels["t678"])
+
+# TODO
+def exp_t6_7_8(opt):
+
+    s = SysTerrier(env)
+
+    if opt == "i":
+
+        print "don't want to spend time indexing, use t678"
+
+    elif opt == "r":
+
+        for t in ["t6", "t7", "t8"]:
+
+            t = Topics(topics["t6"], "d")
+            q = t.query("terrier")
+
+            for m in models:
+                s.retrieve("t678",    "t6."    + m, ["stopwords", "None"],        m, q)
+                s.retrieve("t678.p",  "t6.p."  + m, ["stopwords", "porter"],      m, q)
+                s.retrieve("t678.wp", "t6.wp." + m, ["stopwords", "weak-porter"], m, q)
+                s.retrieve("t678.s",  "t6.s."  + m, ["stopwords", "snowball"],    m, q)
+
+    elif opt == "e":
+
+        for m in models:
+            s.evaluate("t6."    + m, qrels["t6"])
+            s.evaluate("t6.p."  + m, qrels["t6"])
+            s.evaluate("t6.wp." + m, qrels["t6"])
+            s.evaluate("t6.s."  + m, qrels["t6"])
+
 
 def main(argv):
     if len(argv) != 2:
         print "usage: python setup.py <i|r|e>"
         sys.exit(0)
-    exp(argv[1])
+    exp_t678(argv[1])
     
 if __name__ == "__main__":
    main(sys.argv)
