91ba4ab08fb25dd85af6f1a5596912c6ed04c144
Rup <sauparna.palc@gmail.com>
2014-05-09 17:00:23 -0400

    Minor cleanup on shortenqrels.py. exp1.py has new test collection naming convention.

---
+++ src/exp1.py
@@ -0,0 +1,98 @@
+import sys, os
+from SysTerrier import *
+from SysIndri import *
+from SysLucene import *
+from Topics import Topics
+
+def init(config):
+    path = []
+    with open(config, "r") as f:
+        path = f.readlines()
+    home = path[0].rstrip("\n")
+    out  = path[1].rstrip("\n")
+    home_map = {"doc"     : "doc",
+                "topics"  : "topics",
+                "qrels"   : "qrels",
+                "treceval": "trec_eval.9.0",
+                "lucene"  : "lucene.TREC",
+                "terrier" : "terrier-3.5",
+                "indri"   : "indri-5.6",
+                "utils"   : "utils"}
+    out_map = {"index" : "index",
+               "runs"  : "runs",
+               "evals" : "evals",
+               "attic" : "attic"}
+    env = {}
+    for k in home_map.keys():
+        env[k] = os.path.join(home, home_map[k])
+    for k in out_map.keys():
+        env[k] = os.path.join(out, out_map[k])
+    return env
+
+def exp1(opt, env):
+    models = ["bm25","dfi0", "dirichletlm", "lemurtf_idf", "tf_idf"]
+    stems  = ["n", "p"]
+    doc    = {"t123": os.path.join(env["doc"], "cd12"),
+              "t4":   os.path.join(env["doc"], "cd23"),
+              "t5":   os.path.join(env["doc"], "cd24"),
+              "t678": os.path.join(env["doc"], "cd45-cr")}
+    topics = {"t123": os.path.join(env["topics"], "t123.1-150")
+              "t4":   os.path.join(env["topics"], "topics.201-250"),
+              "t5":   os.path.join(env["topics"], "topics.251-300"),
+              "t678": os.path.join(env["topics"], "t678.301-450")}
+    qrels  = {"t123": os.path.join(env["qrels"], "t123.51-200.cd12"),
+              "t4":   os.path.join(env["qrels"], "qrels.201-250.cd23"),
+              "t5":   os.path.join(env["qrels"], "qrels.251-300.cd24"),
+              "t678": os.path.join(env["qrels"], "t678.301-450.cd45-cr")}
+
+    s = SysTerrier(env)
+    # {"runid": "index topic qrel"}
+    tag = {"t6": "t678 t678 t6",
+           "t7": "t678 t678 t7",
+           "t8": "t678 t678 t8",
+           "t678-fr": "t678-fr t678 t678",
+           "fr94": "fr94 t678 t678",
+           "ziff1": "ziff t123 ziff",
+           "ziff2": "ziff t123 ziff"}
+    if opt == "i":
+        # pull out the index names
+        a = []
+        for i in tag.values():
+            a.append(i.split()[0])
+        index = list(set(a))
+        for i in index:
+            for j in stems:
+                s.index(i+"."+j,  doc[i], ["stop", j])
+    elif opt == "r":
+        for i in tag.keys():
+            i_ = tag[i].split()
+            t = Topics(topics[i_[1]])
+            qid = open(os.path.join(env["topics"],  i+".qid"), "r").read().splitlines()
+            q = t.query("terrier", "d", qid)
+            for j in stems:
+                for k in models:
+                    s.retrieve(i_[0]+"."+j,  i+"."+j+"."+k, ["stop", j], k, q)
+    elif opt == "e":
+        for i in tag.keys():
+            i_ = tag[i].split()
+            for j in stems:
+                for k in models:
+                    s.evaluate(i+"."+j+"."+k, qrels[i_[2]])
+
+def main(argv):
+    if len(argv) != 2:
+        print "usage: python setup.py <i|r|e>"
+        sys.exit(0)
+
+    env = init("config.exp1");
+
+    if not (os.path.exists(env["index"]) 
+            and os.path.exists(env["runs"]) 
+            and os.path.exists(env["evals"])):
+        print "Either one of index, runs or evals directory doesn't exist."
+        sys.exit(0)
+
+    exp1(argv[1], env)
+    
+if __name__ == "__main__":
+   main(sys.argv)
--- src/shortenqrels.py
+++ src/shortenqrels.py
@@ -1,5 +1,5 @@
-# reads in a set of qids, and shortens a qrel to only those topics
-# which are in this set
+# Read in a set of qids, and shorten a qrel to only those topics which
+# are in this set.
 
 import sys, os
 
@@ -12,19 +12,19 @@ def read_qid(f):
 
 def main(argv):
     home    = os.environ["HOME"]
-    qid = {"ziff1": read_qid(os.path.join(home, "ir/topics/ziff1.qid")),
-           "ziff2": read_qid(os.path.join(home, "ir/topics/ziff2.qid")),
-           "fr94": read_qid(os.path.join(home, "ir/topics/fr94.qid")),
+    qid = {"ziff1":   read_qid(os.path.join(home, "ir/topics/ziff1.qid")),
+           "ziff2":   read_qid(os.path.join(home, "ir/topics/ziff2.qid")),
+           "fr94":    read_qid(os.path.join(home, "ir/topics/fr94.qid")),
            "t678-fr": read_qid(os.path.join(home, "ir/topics/t678-fr.qid")),
-           "t6": read_qid(os.path.join(home, "ir/topics/t6.qid")),
-           "t7": read_qid(os.path.join(home, "ir/topics/t7.qid")),
-           "t8": read_qid(os.path.join(home, "ir/topics/t8.qid"))}
+           "t6":      read_qid(os.path.join(home, "ir/topics/t6.qid")),
+           "t7":      read_qid(os.path.join(home, "ir/topics/t7.qid")),
+           "t8":      read_qid(os.path.join(home, "ir/topics/t8.qid"))}
 
     spin = ["|","/","-","|","-","\\"]
 
     l_t123 = open(os.path.join(home, "ir/qrels/qrels.1-150")).readlines()
-    ziff1 = open(os.path.join(home, "ir/qrels/qrels.ziff1"), "w")
-    ziff2 = open(os.path.join(home, "ir/qrels/qrels.ziff2"), "w")
+    ziff1  = open(os.path.join(home, "ir/qrels/qrels.ziff1"), "w")
+    ziff2  = open(os.path.join(home, "ir/qrels/qrels.ziff2"), "w")
     for i in range(len(l_t123)):
         print spin[i%5] + "\r",
         q = int(l_t123[i].split()[0])
@@ -36,12 +36,12 @@ def main(argv):
     ziff1.close()
     del(l_t123)
 
-    l_t678 = open(os.path.join(home, "ir/qrels/qrels.301-450")).readlines()
-    fr94 = open(os.path.join(home, "ir/qrels/qrels.fr94"), "w")
+    l_t678  = open(os.path.join(home, "ir/qrels/qrels.301-450")).readlines()
+    fr94    = open(os.path.join(home, "ir/qrels/qrels.fr94"), "w")
     t678_fr = open(os.path.join(home, "ir/qrels/qrels.t678-fr"), "w")
-    t6 = open(os.path.join(home, "ir/qrels/qrels.t6"), "w")
-    t7 = open(os.path.join(home, "ir/qrels/qrels.t7"), "w")
-    t8 = open(os.path.join(home, "ir/qrels/qrels.t8"), "w")
+    t6      = open(os.path.join(home, "ir/qrels/qrels.t6"), "w")
+    t7      = open(os.path.join(home, "ir/qrels/qrels.t7"), "w")
+    t8      = open(os.path.join(home, "ir/qrels/qrels.t8"), "w")
     for i in range(len(l_t678)):
         print spin[i%5] + "\r",
         q = int(l_t678[i].split()[0])
