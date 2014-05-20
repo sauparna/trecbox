cebd560ca5bbd31a81711f0eb65c2b152b7ead7a
Rup <sauparna.palc@gmail.com>
2014-05-20 12:29:24 -0400

    Configurationa and experiment layout handled more neatly.

--- src/trecbox.py
+++ src/trecbox.py
@@ -5,56 +5,60 @@ from SysIndri import *
 from SysLucene import *
 from Topics import Topics
 
-def init(config):
-    path = []
-    with open(config, "r") as f:
-        path = f.readlines()
-    home = path[0].rstrip("\n")
-    out  = path[1].rstrip("\n")
-    home_map = {"doc"     : "doc",
-                "topics"  : "topics",
-                "qrels"   : "qrels",
-                "treceval": "trec_eval.9.0",
-                "lucene"  : "lucene.TREC",
-                "terrier" : "terrier-3.5",
-                "indri"   : "indri-5.6",
-                "utils"   : "utils"}
-    out_map = {"index" : "index",
-               "runs"  : "runs",
-               "evals" : "evals",
-               "attic" : "attic"}
-    env = {}
-    for k in home_map.keys():
-        env[k] = os.path.join(home, home_map[k])
-    for k in out_map.keys():
-        env[k] = os.path.join(out, out_map[k])
-    return env
+def init(f, f1):
+    layout  = json.loads(open(f, "r").read())
+    path   = json.loads(open(f1, "r").read())
 
-def init1(exp):
-    layout = json.loads(open(exp, "r").read())
-    return layout
+    path["o_base"] = os.path.join(path["base"], f)
+    for k in path["in"].keys():
+        path["in"][k] = os.path.join(path["base"], path["in"][k])
+    for k in path["out"].keys():
+        path["out"][k] = os.path.join(path["o_base"], path["out"][k])
 
-def run(opt, env, exp):
+    # # DEBUG
+    # print json.dumps(path, sort_keys=True, indent=4 * ' ')
+    
+    # time-stamp and stow away an existing experiment directory 
 
-    matrix = exp["matrix"]
-    models = exp["models"]
-    stems = exp["stems"]
+    if os.path.exists(path["o_base"]):
+        os.rename(path["o_base"], 
+                  os.path.join(path["in"]["attic"], f + "-" + str(time.time())))
 
-    s = SysTerrier(env)
+    # create the output directories
 
+    os.mkdir(path["o_base"])
+    for k in path["out"].keys():
+        os.mkdir(path["out"][k])
+
+    # remove the the nesting in the path dict
+
+    path.update(dict(path["in"].items() + path["out"].items()))
+    del(path["in"])
+    del(path["out"])
+
+    # # DEBUG
+    # print json.dumps(path, sort_keys=True, indent=4 * ' ')
+
+    return layout, path
+
+def run(opt, layout, path):
+    matrix = layout["matrix"]
+    models = layout["models"]
+    stems  = layout["stems"]
+    s = SysTerrier(path)
     if opt == "i":
         doc = []
         for i in matrix.keys():
             doc.append(matrix[i][0])
         doc = list(set(doc))
         for d in doc:
-            d_path = os.path.join(env["doc"], d)
+            d_path = os.path.join(path["doc"], d)
             for j in stems:
                 s.index(d+"."+j, d_path, ["stop", j])
     elif opt == "r":
         for i in matrix.keys():
             d = matrix[i][0]
-            t_path = os.path.join(env["topics"], matrix[i][1])
+            t_path = os.path.join(path["topic"], matrix[i][1])
             t = Topics(t_path)
             q = t.query("terrier", "d")
             for j in stems:
@@ -62,7 +66,7 @@ def run(opt, env, exp):
                     s.retrieve(d+"."+j,  i+"."+j+"."+k, ["stop", j], k, q)
     elif opt == "e":
         for i in matrix.keys():
-            qrel_path = os.path.join(env["qrels"], matrix[i][2])
+            qrel_path = os.path.join(path["qrel"], matrix[i][2])
             for j in stems:
                 for k in models:
                     s.evaluate(i+"."+j+"."+k, qrel_path)
@@ -72,19 +76,8 @@ def main(argv):
         print "usage: python setup.py <i|r|e>"
         sys.exit(0)
 
-    env = init("config.exp1b");
-    exp = init1("exp1b")
-
-    # DEBUG
-    # print json.dumps(exp, sort_keys=True, indent=4 * ' ')
-
-    if not (os.path.exists(env["index"]) 
-            and os.path.exists(env["runs"]) 
-            and os.path.exists(env["evals"])):
-        print "Either one of index, runs or evals directory doesn't exist."
-        sys.exit(0)
-
-    run(argv[1], env, exp)
+    layout, path = init("exp1b", "conf");
+    run(argv[1], layout, path)
     
 if __name__ == "__main__":
    main(sys.argv)
