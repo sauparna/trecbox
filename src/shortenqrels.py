230863485a6b1e7651ae638aa483059935327069
Rup <sauparna.palc@gmail.com>
2014-05-22 16:53:47 -0400

    Names cleaned up and corrected to match the patters used by tset.py. Since the 30-topics set just belong to TREC 6,7 and 8 and not to any of these years separately in particular, names have been changed to a, b, c.

--- src/shortenqrels.py
+++ src/shortenqrels.py
@@ -16,15 +16,16 @@ def get_qid_list(f):
 
 def main(argv):
     home = os.environ["HOME"]
-    qid = {"ziff1":   get_qid_list(os.path.join(home, "ir/topic/ziff1.30")),
-           "ziff2":   get_qid_list(os.path.join(home, "ir/topic/ziff2.30")),
-           "fr94":    get_qid_list(os.path.join(home, "ir/topic/fr94.30")),
-           "t678-cr-fr": get_qid_list(os.path.join(home, "ir/topic/t678-cr-fr.30")),
-           "t6":      get_qid_list(os.path.join(home, "ir/topic/t6.30")),
-           "t7":      get_qid_list(os.path.join(home, "ir/topic/t7.30")),
-           "t8":      get_qid_list(os.path.join(home, "ir/topic/t8.30"))}
+    qid = {"ziff1": get_qid_list(os.path.join(home, "ir/topic/ziff1.30")),
+           "ziff2": get_qid_list(os.path.join(home, "ir/topic/ziff2.30")),
+           "t-fr" : get_qid_list(os.path.join(home, "ir/topic/301-450-fr.30")),
+           "t.fr" : get_qid_list(os.path.join(home, "ir/topic/301-450.fr.30")),
+           "t.a"  : get_qid_list(os.path.join(home, "ir/topic/301-450.a.30")),
+           "t.b"  : get_qid_list(os.path.join(home, "ir/topic/301-450.b.30")),
+           "t.c"  : get_qid_list(os.path.join(home, "ir/topic/301-450.c.30"))}
 
     i_ziff  = open(os.path.join(home, "ir/qrel/1-100.cd12")).readlines()
+
     o_ziff1 = open(os.path.join(home, "ir/qrel/ziff1.cd12.30"), "w")
     o_ziff2 = open(os.path.join(home, "ir/qrel/ziff2.cd12.30"), "w")
     for i in range(len(i_ziff)):
@@ -35,41 +36,34 @@ def main(argv):
             o_ziff2.write(i_ziff[i])
     o_ziff2.close()
     o_ziff1.close()
+
     del(i_ziff)
 
-    i_t678 = open(os.path.join(home, "ir/qrel/301-450.cd45")).readlines()
-    o_t6   = open(os.path.join(home, "ir/qrel/t6.cd45.30"), "w")
-    for i in range(len(i_t678)):
-        q = int(i_t678[i].split()[0])
-        if q in qid["t6"]:
-            o_t6.write(i_t678[i])
-    o_t6.close()
-    del(i_t678)
-
-    i_t678_cr_fr = open(os.path.join(home, "ir/qrel/301-450.cd45-cr-fr")).readlines()
-    o_t678_cr_fr = open(os.path.join(home, "ir/qrel/t678-cr-fr.cd45-cr-fr.30"), "w")
-    for i in range(len(i_t678_cr_fr)):
-        q = int(i_t678_cr_fr[i].split()[0])
-        if q in qid["t678-cr-fr"]:
-            o_t678_cr_fr.write(i_t678_cr_fr[i])
-    o_t678_cr_fr.close()
-    del(i_t678_cr_fr)
-
     i_t678_cr = open(os.path.join(home, "ir/qrel/301-450.cd45-cr")).readlines()
-    o_fr94    = open(os.path.join(home, "ir/qrel/fr94.cd45-cr.30"), "w")
-    o_t7      = open(os.path.join(home, "ir/qrel/t7.cd45-cr.30"), "w")
-    o_t8      = open(os.path.join(home, "ir/qrel/t8.cd45-cr.30"), "w")
+
+    o_t678_fr = open(os.path.join(home, "ir/qrel/301-450-fr.cd45-cr.30"), "w")
+    o_fr      = open(os.path.join(home, "ir/qrel/301-450.fr.cd45-cr.30"), "w")
+    o_a       = open(os.path.join(home, "ir/qrel/301-450.a.cd45-cr.30"), "w")
+    o_b       = open(os.path.join(home, "ir/qrel/301-450.b.cd45-cr.30"), "w")
+    o_c       = open(os.path.join(home, "ir/qrel/301-450.c.cd45-cr.30"), "w")
     for i in range(len(i_t678_cr)):
         q = int(i_t678_cr[i].split()[0])
-        if q in qid["fr94"]:
-            o_fr94.write(i_t678_cr[i])
-        if q in qid["t7"]:
-            o_t7.write(i_t678_cr[i])
-        if q in qid["t8"]:
-            o_t8.write(i_t678_cr[i])
-    o_t8.close()
-    o_t7.close()
-    o_fr94.close()
+        if q in qid["t-fr"]:
+            o_t678_fr.write(i_t678_cr[i])
+        if q in qid["t.fr"]:
+            o_fr.write(i_t678_cr[i])
+        if q in qid["t.a"]:
+            o_a.write(i_t678_cr[i])
+        if q in qid["t.b"]:
+            o_b.write(i_t678_cr[i])
+        if q in qid["t.c"]:
+            o_c.write(i_t678_cr[i])
+    o_c.close()
+    o_b.close()
+    o_a.close()
+    o_fr.close()
+    o_t678_fr.close()
+
     del(i_t678_cr)
 
 if __name__ == "__main__":
