0757fee5fdf6621d78cfb03a4bfd8bfb98fef972
Sauparna Palchowdhury <rup@bhuto.local>
2014-12-03 12:51:12 -0500

    Modified to use the new configuration stratedgy. Syntactic changes: .keys() dropped. Design edits: order of variables being passed and returned is fixed.

--- trecbox.py
+++ trecbox.py
@@ -8,12 +8,26 @@ from Topics import Topics
 def init(cf, pf):
     # bootstrap paths
     # the name of the plan file will be the name of the output directory
+
+    plan = json.loads(open(pf, "r").read())
+
     name = os.path.basename(pf)
     path = json.loads(open(cf, "r").read())
+    i_exp = os.path.join(path["i_base"], path["in"]["exp"])
+    if not os.path.exists(i_exp):
+        print("Error: Experiment " + name + " doesn't exist in " + i_exp)
+        print("Quitting.")
+    
+    for k in path["in"]:
+        path["in"][k] = os.path.join(path["i_base"], path["in"][k])
+
+    # overwrite
+    path["in"]["topic"] = os.path.join(i_exp, name, "topics")
+    path["in"]["qrel"]  = os.path.join(i_exp, name, "qrels")
+    path["in"]["doc"]  = os.path.join(i_exp, name, "docs")
+
     path["o_base"] = os.path.join(path["o_base"], name)
-    for k in path["in"].keys():
-        path["in"][k] = os.path.join(path["base"], path["in"][k])
-    for k in path["out"].keys():
+    for k in path["out"]:
         path["out"][k] = os.path.join(path["o_base"], path["out"][k])
         if not os.path.exists(path["out"][k]):
             os.makedirs(path["out"][k])
@@ -24,8 +38,7 @@ def init(cf, pf):
     del(path["in"])
     del(path["out"])
 
-    plan = json.loads(open(pf, "r").read())
-    return plan, path
+    return path, plan
 
 def main(argv):
 
@@ -33,10 +46,11 @@ def main(argv):
         print("USAGE: python trecbox.py <conf file> <plan file>")
         sys.exit(0)
     
-    plan, path = init(argv[1], argv[2]);
+    path, plan = init(argv[1], argv[2]);
 
-    # print(json.dumps(plan, sort_keys=True, indent=4 * ' '))
-    # print(json.dumps(path, sort_keys=True, indent=4 * ' '))
+    # # DEBUG
+    # print(json.dumps(path, sort_keys=True, indent=2))
+    # print(json.dumps(plan, sort_keys=True, indent=2))
     # sys.exit(0)
 
     irsys_ = {"terrier": SysTerrier(path), 
@@ -47,7 +61,7 @@ def main(argv):
     stems  = plan["stems"]
     irsys  = irsys_[plan["system"]]
 
-    for i in matrix.keys():
+    for i in matrix:
         d  = matrix[i][0]
         dp = os.path.join(path["doc"], d)
         t_ = matrix[i][1].split(":")
