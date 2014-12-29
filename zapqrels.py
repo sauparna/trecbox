0e1af65cb1f273edc16235461bbaa164bc02d8bd
Sauparna Palchowdhury <rup@bhuto.local>
2014-12-29 01:45:48 -0500

    shortened, prints to stdout

--- zapqrels.py
+++ zapqrels.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 
-# Usage: zapqrels.py <full qrels file> <subset file> <out file>
+# Usage: zapqrels.py <full qrels file> <subset file>
 import sys
 
 def main(argv):
     # docno = {k: None for k in [l.rstrip() for l in open(argv[2], "r")]}
-    qid = {k: None for k in [l.rstrip() for l in open(argv[2], "r")]}
-    fp = open(argv[1], "r")
-    fp_ = open(argv[3], "w")
-    for l in fp:
-        a = l.split()
-        # if a[2] in docno:
-        if a[0] in qid:
-            fp_.write(l)
-    fp_.close()
-    fp.close()
+    subset = {k: None for k in [l.rstrip() for l in open(argv[2], "r")]}
+    with open(argv[1], "r") as fp:
+        for l in fp:
+            a = l.split()
+            # if a[2] in docno:
+            if a[0] in subset:
+                print(l.strip())
 
 if __name__ == "__main__":
     main(sys.argv)
