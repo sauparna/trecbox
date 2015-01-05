70aa7870b36da54c8ea8a9aaf7f3c117d443104d
Sauparna Palchowdhury <rup@bhuto.local>
2015-01-05 06:03:55 -0500

    uses y,v from my computations

--- meta.R
+++ meta.R
@@ -1,5 +1,8 @@
 library(metafor)
 library(xtable)
+library(gridExtra)
+library(gdata)
+
 meta_analysis = function(in_d, out_d)
 {
   path    = list.files(path=in_d, full.names=T)
@@ -22,27 +25,44 @@ meta_analysis = function(in_d, out_d)
     es.D[[i]] = escalc(data=tab[[i]], measure="MD",  m1i=m2,sd1i=s2,n1i=n2, m2i=m1,sd2i=s1,n2i=n1, append=T, var.names=c("y_", "v_"))
     es.R[[i]] = escalc(data=tab[[i]], measure="ROM", m1i=m2,sd1i=s2,n1i=n2, m2i=m1,sd2i=s1,n2i=n1, append=T, var.names=c("y_", "v_"))
 
-    oname = paste(basename(path[i]), "D.RE", "tex", sep=".")
-    ofile = paste(out_d, oname, sep="/")
-    print(xtable(es.D[[i]], caption=basename(path[i]), digits=c(0,0,4,4,0,4,4,0,4,4,4,4,4,4,4,4)), type="latex", file=ofile)
+    ## TODO: table columns have increased.
+    
+    ## ## oname = paste(basename(path[i]), "D.RE", "tex", sep=".")
+    ## oname = paste(basename(path[i]), "D.RE", "tab", "pdf", sep=".")
+    ## ofile = paste(out_d, oname, sep="/")
+    ## ## print(xtable(es.D[[i]], caption=basename(path[i]), digits=c(0,0,4,4,0,4,4,0,4,4,4,4,4,4,4,4)), type="latex", file=ofile)
+    ## pdf(ofile, height=11, width=8.5)
+    ## print(grid.table(es.D[[i]]))
+    ## dev.off()
 
-    oname = paste(basename(path[i]), "R.RE", "tex", sep=".")
-    ofile = paste(out_d, oname, sep="/")
-    print(xtable(es.R[[i]], caption=basename(path[i]), digits=c(0,0,4,4,0,4,4,0,4,4,4,4,4,4,4,4)), type="latex", file=ofile)
+    ## ## oname = paste(basename(path[i]), "R.RE", "tex", sep=".")
+    ## oname = paste(basename(path[i]), "R.RE", "tab", "pdf", sep=".")
+    ## ofile = paste(out_d, oname, sep="/")
+    ## ## print(xtable(es.R[[i]], caption=basename(path[i]), digits=c(0,0,4,4,0,4,4,0,4,4,4,4,4,4,4,4)), type="latex", file=ofile)
+    ## pdf(ofile, height=11, width=8.5)
+    ## print(grid.table(es.R[[i]], gp=gpar(fontsize=7)))
+    ## dev.off()
 
+    oname = paste(basename(path[i]), "R.RE", "txt", sep=".")
+    ofile = paste(out_d, oname, sep="/")
+    write.fwf(es.R[[i]], file=ofile, sep=" ", quote=F, rownames=T)
+    
     # Do the meta-analysis and write summary and plot to file
 
-    ma.D.RE[[i]] = rma.uni(data=es.D[[i]], y_, v_, method="DL")
-    ma.R.RE[[i]] = rma.uni(data=es.R[[i]], y_, v_, method="DL")
+    # NOTE: metafor's v_ differs from v
+    ma.D.RE[[i]] = rma.uni(data=es.D[[i]], yi=y, vi=v, tau2=tab[[i]]$tau2, method="DL")
+    ma.R.RE[[i]] = rma.uni(data=es.R[[i]], yi=y, vi=v, tau2=tab[[i]]$tau2, method="DL")
+    ## ma.D.RE[[i]] = rma.uni(data=es.D[[i]], yi=y_, vi=v_, tau2=tab[[i]]$tau2, method="DL")
+    ## ma.R.RE[[i]] = rma.uni(data=es.R[[i]], yi=y_, vi=v_, tau2=tab[[i]]$tau2, method="DL")
 
     # summary
-    oname = paste(basename(path[i]), "D.RE", "summary", sep=".")
+    oname = paste(basename(path[i]), "D.RE", "s", sep=".")
     ofile = paste(out_d, oname, sep="/")
     sink(file=ofile)
     print(summary(ma.D.RE[[i]]))
     sink(NULL)
     
-    oname = paste(basename(path[i]), "R.RE", "summary", sep=".")
+    oname = paste(basename(path[i]), "R.RE", "s", sep=".")
     ofile = paste(out_d, oname, sep="/")
     sink(file=ofile)
     print(summary(ma.R.RE[[i]]))
@@ -52,14 +72,14 @@ meta_analysis = function(in_d, out_d)
     oname = paste(basename(path[i]), "D.RE", "pdf", sep=".")
     ofile = paste(out_d, oname, sep="/")
     pdf(ofile)
-    print(forest.rma(ma.D.RE[[i]], digit=c(2, 2), refline=0, slab=es.R[[i]]$testcol, mlab="m2-m1, RE", xlab=basename(path[i]), xlim=c(-0.5, 0.5), alim=c(-0.3,0.3)))
+    print(forest.rma(ma.D.RE[[i]], digit=c(2, 2), refline=0, slab=es.R[[i]]$testcol, mlab="m2-m1, RE", xlab=basename(path[i])))
     #print(forest.rma(ma.D.RE[[i]], digit=c(2, 2), slab=es.R[[i]]$testcol))
     dev.off()
     
     oname = paste(basename(path[i]), "R.RE", "pdf", sep=".")
     ofile = paste(out_d, oname, sep="/")
     pdf(ofile)
-    print(forest.rma(ma.R.RE[[i]], digit=c(2, 2),  refline=0, slab=es.R[[i]]$testcol, mlab="log(m2/m1), RE", xlab=basename(path[i]), xlim=c(-3, 1.5), alim=c(-2.5,2.5)))
+    print(forest.rma(ma.R.RE[[i]], digit=c(2, 2),  refline=0, slab=es.R[[i]]$testcol, mlab="log(m2/m1), RE", xlab=basename(path[i])))
     #print(forest.rma(ma.R.RE[[i]], digit=c(2, 2), slab=es.R[[i]]$testcol))
     dev.off()
   }
